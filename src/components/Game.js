3d4fe75445698b63aa5401e4c5c27e931e727c01
LinoyHorvat <linoy.horvat@gmail.com>
2022-01-05 19:00:29 +0200

    first commit

--- src/components/Game.js
+++ src/components/Game.js
@@ -44,7 +44,7 @@ export default function Game() {
       setStyle({ color: "red" });
     }
     if (counter === 0) {
-      // history.push("/results");
+      history.push("/results");
     }
   }, [counter]);
 
